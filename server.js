1c748cb2612ca5c6b51a2481121fb8ab1d5b165f
Miha sposs <mihasposssposs@gmail.com>
2026-03-12 12:20:17 +0100

    Fix contact form and remove ordersCount error

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 const express = require("express");
 const Stripe = require("stripe");
 const cors = require("cors");
-require("dotenv").config();
+
 
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -39,3 +42,37 @@ app.post("/create-checkout-session", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS
+    
+  }
+});
+
+app.post("/send-message", async (req, res) => {
+
+  console.log("Mensaje recibido:", req.body);
+
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: "mihasposssposs@gmail.com",
+    subject: "Nuevo mensaje desde Legendary Racing Keychain",
+    text: `
+Nombre: ${name}
+Email: ${email}
+
+Mensaje:
+${message}
+`
+  };
+
+  await transporter.sendMail(mailOptions);
+
+  res.send("Mensaje enviado correctamente");
+
+});
